0ccea66ed4a3e03ef7e1507cd83b6a05839a664a
rishank9399 <116252920+rishank9399@users.noreply.github.com>
2025-09-21 14:51:58 +0530

    save

--- server/index.js
+++ server/index.js
@@ -1,23 +1,47 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const app = express();
+// require("dotenv").config();
+// const cookieParser = require("cookie-parser");
+// const authRoute = require("./Routes/AuthRoute");
+// const { MONGO_URL, PORT } = process.env;
+
+// mongoose
+//   .connect(MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB is  connected successfully"))
+//   .catch((err) => console.error(err));
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// }); 
+
+// app.use(
+//   cors({
+//     origin: [process.env.CORS_ORIGIN_SITE, "http://localhost:3000"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+// app.use(cookieParser());
+
+// app.use(express.json());
+
+// app.use("/", authRoute); 
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
-require("dotenv").config();
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
 const authRoute = require("./Routes/AuthRoute");
 const { MONGO_URL, PORT } = process.env;
 
-mongoose
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB is  connected successfully"))
-  .catch((err) => console.error(err));
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-}); 
+const app = express();
 
 app.use(
   cors({
@@ -27,7 +51,19 @@ app.use(
   })
 );
 app.use(cookieParser());
-
 app.use(express.json());
 
-app.use("/", authRoute); 
+app.use("/", authRoute);
+
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
